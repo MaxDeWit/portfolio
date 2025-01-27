2f59aa6001949f846912e957b99f202250cb783c
Max <97080691@st.deltion.nl>
2025-01-27 09:19:59 +0100

    Tweede commit

--- DatabaseProject/resources/views/contact.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout>
-    <x-slot:title>Contact</x-slot:title>
-    <h1>Dit is de contact page.</h1>
-</x-layout>
--- DatabaseProject/resources/views/home.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout>
-    <x-slot:title>Home</x-slot:title>
-    <h1>Dit is de home page.</h1>
-</x-layout>
